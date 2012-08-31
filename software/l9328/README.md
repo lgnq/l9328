85a1a0c7afd71a5bf29d30e03cbeb99eed26a2af
yiyue.fang <dzzxzz@gmail.com>
2012-08-31 09:48:13 +0800

    commit a working copy
    blink a LED and add finsh shell

---
+++ software/l9328/README.md
@@ -0,0 +1,7 @@
+PTZ control board
+=================
+
+L9328 PTZ control board
+MCU : STM32103RE
+step motor driver IC : L6470
+RTOS : RT-Thread
---
+++ software/l9328/applications/application.c
@@ -0,0 +1,90 @@
+/*
+ * File      : application.c
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2006, RT-Thread Development Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2009-01-05     Bernard      the first version
+ */
+
+/**
+ * @addtogroup STM32
+ */
+
+/*@{*/
+
+#include <board.h>
+#include <rtthread.h>
+
+#include "led.h"
+
+ALIGN(RT_ALIGN_SIZE)
+static rt_uint8_t led_stack[ 512 ];
+static struct rt_thread led_thread;
+static void led_thread_entry(void *parameter)
+{
+    unsigned int count=0;
+
+    rt_hw_led_init();
+
+    while (1)
+    {
+        /* led1 on */
+#ifndef RT_USING_FINSH
+        rt_kprintf("led on, count : %d\r\n",count);
+#endif
+        count ++;
+        rt_hw_led_on();
+        rt_thread_delay(RT_TICK_PER_SECOND); /* sleep 1 second and switch to other thread */
+
+        /* led1 off */
+#ifndef RT_USING_FINSH
+        rt_kprintf("led off\r\n");
+#endif
+        rt_hw_led_off();
+        rt_thread_delay(RT_TICK_PER_SECOND);
+    }
+}
+
+void rt_init_thread_entry(void *parameter)
+{
+}
+
+int rt_application_init(void)
+{
+    rt_thread_t init_thread;
+
+    rt_err_t result;
+
+    /* init led thread */
+    result = rt_thread_init(&led_thread,
+        "led",
+        led_thread_entry, RT_NULL,
+        (rt_uint8_t *)&led_stack[0], sizeof(led_stack), 20, 5);
+    if (result == RT_EOK)
+    {
+        rt_thread_startup(&led_thread);
+    }
+
+#if (RT_THREAD_PRIORITY_MAX == 32)
+    init_thread = rt_thread_create("init",
+                                rt_init_thread_entry, RT_NULL,
+                                2048, 8, 20);
+#else
+    init_thread = rt_thread_create("init",
+                                rt_init_thread_entry, RT_NULL,
+                                2048, 80, 20);
+#endif
+
+    if (init_thread != RT_NULL)
+        rt_thread_startup(init_thread);
+
+    return 0;
+}
+
+/*@}*/
